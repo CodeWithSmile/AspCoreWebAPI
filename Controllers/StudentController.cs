1b4f0033a4fade165d3c3e3cb6a8e0ab4e6fd115
Muskan <muskanaslamsayyad@gmail.com>
2024-09-27 00:48:01 +0530

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Student_Demo_Crud_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    
+    public class StudentController : ControllerBase
+    {
+        //create empty list
+        public static List<Student> students = new List<Student>();
+        // GET: api/<StudentController>
+        [HttpGet]
+        public IEnumerable<Student> Get()
+        {
+            return students; //empty at initial
+        }
+
+        // GET api/<StudentController>/5
+        [HttpGet("{id}")]
+        public Student Get(int id)
+        {
+            return students.FirstOrDefault(s=>s.Id==id); //check your pass id same as actual id
+        }
+
+        // POST api/<StudentController>
+        [HttpPost]
+        public void Post([FromBody] Student value) //from body convert json into object
+        {
+            students.Add(value);
+        }
+
+        // PUT api/<StudentController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Student value)
+        {
+            int i=students.FindIndex(s=>s.Id==id);
+            if(i>=0)
+            {
+                students[i] = value;
+            }
+        }
+
+        // DELETE api/<StudentController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            students.RemoveAll(s=>s.Id==id);
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,8 @@
+﻿namespace Student_Demo_Crud_Api
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
